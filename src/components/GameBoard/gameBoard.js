d408ac5d8c96820f1cb7aee047e701df2eca23da
ikram mohideen <ikram432k@gmail.com>
2022-05-19 11:48:17 +0530

    src/components/GameBoard/gameBoard.js

--- src/components/GameBoard/gameBoard.js
+++ src/components/GameBoard/gameBoard.js
@@ -4,8 +4,9 @@ import './gameBoard.css'
 function GameBoard(props){
 const{id,url,title} = props.obj;
 return(
-<div className="card" id={id} onClick={props.handleClick} >
-    <img src={url} alt={title}/>
+<div className="card"  >
+    <img src={url} alt={title} id={id} onClick={props.handleClick}/>
+    <p>{title}</p>
 </div>
 )
 }
